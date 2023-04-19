f878a647d1fb7c2fdc4152e166803e68fe46ac68
Top Intelligence Academy <jean.machado@topintelligence.com.br>
2023-04-19 12:05:16 -0300

    Comando básico para demonstrar IDEs

---
+++ 02_Instalacao_Anaconda_IDEs.py
@@ -0,0 +1,16 @@
+# https://www.anaconda.com/
+
+# Cria lista com 4 elementos
+info=['Aula','Top','Intelligence','Academy']
+
+# Aplica ordenação
+info.sort()
+
+# Imprime na tela
+print(info)
+
+# Altera valor do primeiro registro de índice zero
+info[0]='Aprendendo Python';
+
+# Imprime lista atualizada
+print(info)
